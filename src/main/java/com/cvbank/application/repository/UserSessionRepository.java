dd25f71039f860aba93519ee8c8dd9ad7145d7cd
Shurick <sacha11@list.ru>
2019-02-26 23:41:26 +0200

    JobSeeker

--- src/main/java/com/cvbank/application/repository/UserSessionRepository.java
+++ src/main/java/com/cvbank/application/repository/UserSessionRepository.java
@@ -1,7 +1,10 @@
 package com.cvbank.application.repository;
 
+import com.cvbank.application.entity.User;
 import com.cvbank.application.entity.UserSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserSessionRepository extends JpaRepository<UserSession,Integer> {
+public interface UserSessionRepository extends JpaRepository<UserSession, Integer> {
+	 User findUserByToken(String token);
+	
 }
